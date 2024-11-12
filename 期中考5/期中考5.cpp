7f6f79725bc48fa2dfe2394b1fd2752c6be8b519
hp <hp@LAPTOP-7MBQV5A4>
2024-11-12 23:51:46 +0800

    加入專案檔案。

---
+++ 期中考5/期中考5.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+    if (n == 0) return 1;
+    else return n * factorial(n - 1);
+}
+
+int main()
+{
+    int num;
+
+    cout << "輸入一個正整數: ";
+    cin >> num;
+
+    int result = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        result *= i;
+    }
+    cout << "非遞迴版本: " << num << "! =" << result << endl;
+
+
+    result = factorial(num);
+    cout << "遞迴版本: " << num << "! = " << result << endl;
+}
+
+
